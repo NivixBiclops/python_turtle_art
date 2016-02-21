85b035908665ab6db260b9176c45d4202875d359
NivixBiclops <jaqlai888@gmail.com>
2016-02-21 17:58:41 +0800

    Update Spiral.py

--- Spiral.py
+++ Spiral.py
@@ -1,3 +1,4 @@
+
 import turtle, random
 turtle.colormode(255)
 t = turtle.Turtle()
@@ -5,13 +6,11 @@ t.home()
 randint = random.randint
 var1 = 0
 t.speed("fastest")
-for x in range (50):
+for x in range(50):
 	r = randint(0,255)
 	g = randint(0,255)
 	b = randint(0,255)
-	turtle.color(r, g, b)
-	var1 = var1 + 10
+	t.pencolor(r, g, b)
+	var1 += 10
 	t.circle(var1,180)
 	t.width(var1/20)
-
-    
