889ce8b33c174e7959ce9dfae374d42cad94101b
NivixBiclops <jaqlai888@gmail.com>
2016-02-21 17:55:58 +0800

    Update Spiral.py

--- Spiral.py
+++ Spiral.py
@@ -7,8 +7,8 @@ var1 = 0
 t.speed("fastest")
 for x in range (50):
 	r = randint(0,255)
-	r = randint(0,255)
-	r = randint(0,255)
+	g = randint(0,255)
+	b = randint(0,255)
 	turtle.color(r, g, b)
 	var1 = var1 + 10
 	t.circle(var1,180)
